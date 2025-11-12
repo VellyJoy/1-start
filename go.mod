89434fc52eb5c35534b735250a76e160ea8ed1e4
Мак <mak@MacBook-Pro-Mak.local>
2025-11-12 22:45:27 +0300

    Готовый калькулятор

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VellyJoy/2-calc
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,61 @@
+package main 
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+	
+func main() {
+var operation string 
+var input string 
+
+fmt.Println ("Введите тип операции (SUM,AVG,MED):")
+fmt.Scanln(&operation)
+
+fmt.Println ("Введите числа через запятую:")
+fmt.Scanln (&input)
+
+parts := strings.Split (input, ",")
+
+var numbers []float64
+for _, p := range parts {
+	num , err := strconv.ParseFloat(strings.TrimSpace(p), 64)
+	if err != nil {
+		fmt.Println ("Введено неверное число:", p)
+		return
+	}
+	numbers = append(numbers, num)
+}
+
+switch strings.ToUpper(operation) {
+case "SUM":
+	sum := 0.0 
+	for _, n := range numbers {
+		sum += n 
+	}
+	fmt.Println ("Сумма:", sum)
+
+case "AVG":
+	sum := 0.0 
+	for _, n := range numbers {
+		sum += n 
+	}
+	fmt.Println ("Среднее число:", sum / float64(len(numbers)))
+
+case "MED": 
+sort.Float64s(numbers)
+n := len(numbers)
+if n%2 == 0 {
+	fmt.Println ("Медиана равна:", (numbers[n/2-1]+numbers[n/2])/2)
+} else {
+	fmt.Println("Медиана равна:", numbers[n/2])
+}
+default:
+	fmt.Println ("Неизвестная операция! Используйте операции из списка.")
+}
+}
+
+
+
